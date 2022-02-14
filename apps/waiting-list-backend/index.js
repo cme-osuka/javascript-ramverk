ac7555078a4702cdaff195c603a663cf408643bf
Oscar Nilsson <oscar@osuka.dev>
2022-02-14 15:13:00 +0100

    changes

--- apps/waiting-list-backend/index.js
+++ apps/waiting-list-backend/index.js
@@ -6,26 +6,50 @@ const app = express();
 const server = http.createServer(app);
 
 const { Server } = socketio;
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "*"
+  }
+});
 
-const messages = [];
-const recentMessages = messages.slice(Math.max(messages.length - 20, 0));
+const waitingList = [];
+const recent = waitingList.slice(Math.max(waitingList.length - 20, 0));
+
+function id() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+/**
+ * Msg:
+ * timestamp: date
+ * name: string
+ * room: string
+ * color: string
+ */
 
 app.get("/", (req, res) => {
-  res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
 io.on("connection", (socket) => {
   console.log("A user connected");
 
-  socket.on("chatmsg", (msg) => {
-    if (typeof msg === "string") {
-      console.log("message: ", msg);
-      messages.push(msg);
-      io.emit("newmsg", recentMessages);
+  socket.on("help", (req) => {
+    if (!req.timestamp || !req.name || !req.room || !req.color) {
+      return socket.emit("error", "Must send timestamp, name, room");
     }
+
+    waitingList.push({
+      id: id(),
+      name: req.name,
+      timestamp: req.timestamp,
+      room: req.room,
+      color: req.color,
+    });
+    io.emit("new", waitingList);
   });
 });
+
+io.listen(8001, () => {});
 
 server.listen(8000, () => {
   console.log("listening on port :8000");
