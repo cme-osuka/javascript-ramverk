c1c1754316858675b659931d467112595f4e7a18
Oscar Nilsson <oscar@osuka.dev>
2022-02-14 13:43:34 +0100

    reset client

--- apps/waiting-list-client/src/App.js
+++ apps/waiting-list-client/src/App.js
@@ -1,22 +1,10 @@
-import NameRegistration from "./components/NameRegistration";
-import List from "./components/List";
+
 import "./App.css";
 
-
 function App() {
-  const waitinglist = ["oscar", "bob"];
-
-  function handleClick() {
-    console.log("Click från App");
-  }
 
   return (
     <div>
-      <NameRegistration
-        placeholder="Ditt namn från App"
-        handleClick={handleClick}
-      />
-      <List data={waitinglist} />
     </div>
   );
 }
--- apps/waiting-list-client/src/components/Button.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Button(props) {
-  return <button onClick={props.handleClick}>{props.text}</button>
-}
-
-export default Button
